85353e72bf4577e7fdd819444c3d7a4e75b1bf00
Carles Andres <carlos.andres.c@gmail.com>
2024-07-21 16:33:52 +0100

    Fix types

--- apps/oldmineral/components/TextInput.tsx
+++ apps/oldmineral/components/TextInput.tsx
@@ -1,16 +1,14 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes } from "react";
 
 const defaultStyles = `border-b border-transparent bg-transparent font-mono
 p-2 hover:border-gray-500 form-control cursor-pointer text-base font-bold
 dark:text-gray-300 print:border-none print:text-black`;
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  className?: string;
-}
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 
 const TextInput = (props: Props) => {
-  const { className = defaultStyles } = props;
-  return <input type="text" className={className} {...props} />;
+  const { className = defaultStyles, ...rest } = props;
+  return <input type="text" className={className} {...rest} />;
 };
 
 export default TextInput;
