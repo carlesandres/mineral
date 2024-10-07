fecb7341b4b57a870ea4669de4bf02604babb9eb
Carles Andres <carlos.andres.c@gmail.com>
2024-10-07 22:32:10 +0100

    Move non-blog out of nextra

--- apps/carlesandres/nextra/theme.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextraThemeLayoutProps } from 'nextra'
-import '../src/styles/main.css';
-import Layout from '../src/components/Layout';
- 
-export default function NextraLayout({ children }: NextraThemeLayoutProps) {
-  return <Layout>{children}</Layout>
-}
