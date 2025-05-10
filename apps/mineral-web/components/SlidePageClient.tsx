17ed79441d6bca3a775e28246467a8399a78b5b6
Carles Andres <carlos.andres.c@gmail.com>
2025-05-10 22:44:20 +0100

    Updated deps

--- apps/mineral-web/components/SlidePageClient.tsx
+++ apps/mineral-web/components/SlidePageClient.tsx
@@ -3,20 +3,49 @@
 import { useSearchParams } from 'next/navigation';
 import { notFound } from 'next/navigation';
 import useNotesStore, { getNoteById } from 'hooks/useNotesStore';
-import { useEffect } from 'react';
-import { Deck, Slide } from 'spectacle';
+import { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import rehypeSlug from 'rehype-slug';
 import useSettingsStore from '@/hooks/useSettingsStore';
 import remarkGfm from 'remark-gfm';
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from './ui/carousel';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 const SlidePageClient = () => {
   const { initialized } = useNotesStore((state) => state);
   const searchParams = useSearchParams();
   const noteId = searchParams?.get('id');
   const { gfm } = useSettingsStore();
+  const [api, setApi] = useState<CarouselApi | null>(null);
 
   const remarkPlugins = gfm ? [remarkGfm] : [];
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowLeft') {
+        api.scrollPrev();
+      }
+
+      if (event.key === 'ArrowRight') {
+        api.scrollNext();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [api]);
 
   if (!noteId) {
     notFound();
@@ -54,21 +83,27 @@ const SlidePageClient = () => {
   );
 
   return (
-    <div className="relative flex h-screen print:h-auto">
-      <Deck>
-        {slideContents.map((content, index) => (
-          <Slide key={index} className="overflow-y-auto p-20 pb-0">
-            <div className="prose w-full max-w-none overflow-y-scroll dark:prose-invert prose-code:before:content-none prose-code:after:content-none print:text-gray-950 print:prose-headings:text-gray-950 print:prose-a:text-gray-950">
-              <Markdown
-                rehypePlugins={[rehypeSlug]}
-                remarkPlugins={remarkPlugins}
+    <div className="mx-auto flex h-full w-full items-center justify-center bg-blue-100 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+      <AspectRatio ratio={16 / 9} className="overflow-hidden p-20">
+        <Carousel className="mx-auto w-full" setApi={setApi}>
+          <CarouselContent className="flex gap-4">
+            {slideContents.map((content, index) => (
+              <CarouselItem
+                key={index}
+                className="overflow-rounded-lg mb-20 h-full bg-red-500"
               >
-                {content}
-              </Markdown>
-            </div>
-          </Slide>
-        ))}
-      </Deck>
+                <Markdown
+                  className="prose w-full max-w-none dark:prose-invert prose-code:before:content-none prose-code:after:content-none print:text-gray-950 print:prose-headings:text-gray-950 print:prose-a:text-gray-950"
+                  rehypePlugins={[rehypeSlug]}
+                  remarkPlugins={remarkPlugins}
+                >
+                  {content}
+                </Markdown>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </AspectRatio>
     </div>
   );
 };
